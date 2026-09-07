5d6bf68b6fb0eba711cf4af13e150b64743d936e
zchovankyo <morecek555698@gmail.com>
2026-09-07 16:00:43 +0200

    Update send.js

--- valeocars.ic/api/send.js
+++ valeocars.ic/api/send.js
@@ -1,4 +1,18 @@
 export default async function handler(req, res) {
+  // Nastavení CORS hlaviček, aby Vercel požadavek neblokoval
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
